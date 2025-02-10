e49fc812a64f7b90fd160cd9a78e6e9eee435080
hand3seen <102429692+hand3seen@users.noreply.github.com>
2025-02-10 15:18:18 -0500

    Update particley.js

--- particley.js
+++ particley.js
@@ -13,7 +13,7 @@ function setup() {
   flowfield = new Array(cols * rows);
 
   // Create particles
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < 1000; i++) {
     particles[i] = new Particle();
   }
 
